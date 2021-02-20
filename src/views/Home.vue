62cc2fe7e7d79677219a45a2060ec287f8079428
kuguats <445786355@qq.com>
2021-02-20 16:19:46 +0800

    首页热门剧情、最新帖子

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -86,7 +86,38 @@
                 <span> 热门剧情</span>
             </template>
             <template slot="body">
-                <!-- TODO:热门剧情滚动slider -->
+              <div class="m-hot-slots">
+                <el-carousel
+                    height="66px"
+                    direction="vertical"
+                    indicator-position="none"
+                    v-if="hot_plots && hot_plots.length"
+                >
+                  <el-carousel-item v-for="(items, key) in hot_plots" :key="key" class="m-carousel">
+                    <el-row :gutter="20">
+                      <template v-for="(item, k) in items">
+                        <el-col :md="8" v-if="item" :key="k">
+                          <router-link
+                              class="u-item"
+                              :to="{name:'view',params:{source_id:item.id}}"
+                          >
+                            <div class="u-icon">
+                                <img :src="hot_plot_icon" />
+                            </div>
+                            <div class="m-carousel-content">
+                              <div class="u-title" v-text="item.name"></div>
+                              <div class="u-desc" v-if="item.post">
+                                <span :title="item.post.remark">{{'By ' + item.post.user_nickname}}</span>
+                              </div>
+                            </div>
+                          </router-link>
+                        </el-col>
+                      </template>
+                    </el-row>
+                  </el-carousel-item>
+                </el-carousel>
+                <div v-else style="text-align:center">😂 暂无热门事件</div>
+              </div>
             </template>
         </WikiPanel>
 
@@ -119,18 +150,47 @@
                 <span> 最近更新</span>
             </template>
             <template slot="body">
-                <!-- TODO:最新作品 -->
+              <div class="m-posts">
+                <div class="m-post" v-for="(post, key) in newest_posts" :key="key">
+                  <div class="m-head">
+                    <div class="m-user">
+                      <div class="u-author">
+                        <img class="u-icon" :src="post.user_avatar" :alt="post.user_nickname"/>
+                        <a :href="post.user_id ? author_url(post.user_id) : null" class="u-name"
+                           v-text="post.user_nickname"></a>
+                      </div>
+                      <div class="u-updated" v-text="ts2str(post.updated)"></div>
+                    </div>
+                    <div class="m-source">
+                      <div class="u-source">
+                        <img class="u-icon" svg-inline src="../assets/img/knowledge.svg" />
+                        <router-link class="u-name" :to="{name: 'view', params: { source_id: post.source_id }}"
+                                     v-text="post.title"></router-link>
+                      </div>
+                      <div class="u-remark" v-if="post.remark" v-text="'📑 ' + post.remark"></div>
+                    </div>
+                  </div>
+                  <div class="m-body">
+                    <div class="u-excerpt" :to="{ name: 'view', params: { source_id: post.source_id } }"
+                         v-html="ellipsis(post.excerpt)"></div>
+                  </div>
+                </div>
+              </div>
             </template>
         </WikiPanel>
     </div>
 </template>
 
 <script>
+const { JX3BOX } = require("@jx3box/jx3box-common");
 import { feedback } from "@jx3box/jx3box-common/js/jx3box.json";
 import Search from "../components/Search";
 import WikiPanel from "@jx3box/jx3box-common-ui/src/WikiPanel";
+import {WikiPost} from "@jx3box/jx3box-common/js/helper";
 import { getMenuGroups } from "@/service/helper.js";
 import { get_list } from "@/service/knowledge.js";
+import {getStatRank} from "@jx3box/jx3box-common/js/stat";
+import {authorLink, ts2str, iconLink} from "@jx3box/jx3box-common/js/utils";
 export default {
     name: "Home",
     props: [],
@@ -138,10 +198,31 @@ export default {
         return {
             feedback,
             menu_groups: {},
+            hot_plots: null,
+            hot_plot_views: null,
+            hot_plot_icon: JX3BOX.__iconPath + "icon/243.png",
             jargons: [],
+            newest_posts: null,
         };
     },
     methods: {
+        icon_url: iconLink,
+        author_url: authorLink,
+        ts2str,
+        chuck(arr, number = 3) {
+          let output = [];
+          for (let i = 0; i < arr.length; i += number) {
+            output.push(arr.slice(i, i + number));
+          }
+          return output;
+        },
+        ellipsis(value) {
+          value = value ? value.trim() : "";
+          if (value.length > 100) {
+            return value.slice(0, 100) + "...";
+          }
+          return value;
+        },
         loadGames: function() {
             getMenuGroups({
                 names: ["guide-pve", "guide-pvx", "guide-pvp"],
@@ -165,12 +246,43 @@ export default {
                 this.jargons = res.data.data.data;
             });
         },
+        loadHotPlots: function() {
+          getStatRank('plot').then((res) => {
+            res = res.data;
+            this.hot_plot_views = [];
+            let source_ids = [];
+            res.forEach((item) => {
+              if (item.name.startsWith("plot")) {
+                let id = item.name.split("-").pop();
+                source_ids.push(id);
+                this.hot_plot_views[id] = item.value;
+              }
+            });
+
+            get_list({ids: source_ids}).then((res) => {
+              res = res.data;
+              // 按照长度分批
+              if (res.code === 200) this.hot_plots = this.chuck(Object.values(res.data.data));
+            });
+          });
+        },
+        loadNewestPost() {
+          // 获取最新帖子列表
+          WikiPost.newests('knowledge').then(
+              (res) => {
+                res = res.data;
+                if (res.code === 200) this.newest_posts = res.data.newest;
+              }
+          );
+        },
         loadData: function() {
-            this.loadGames();
-            this.loadJargon();
+          this.loadHotPlots();
+          this.loadGames();
+          this.loadJargon();
+          this.loadNewestPost();
         },
     },
-    mounted: function() {
+    created() {
         this.loadData();
     },
     components: {
