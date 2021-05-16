277ab9570579e8db5c71eeb113357ea5a90cd1d6
Kuguats <445786355@qq.com>
2021-05-17 00:00:04 +0800

    fix

--- src/driver/knowledge.js
+++ src/driver/knowledge.js
@@ -3,12 +3,14 @@ Vue.config.productionTip = false;
 // 第三方UI组件
 import Vue from "vue";
 import ElementUI from "element-ui";
+
 Vue.use(ElementUI);
 
 // 通用UI模块
 import JX3BOX_UI from '@jx3box/jx3box-common-ui'
 import "@jx3box/jx3box-common/css/element.css";
 import "@jx3box/jx3box-common/css/normalize.css";
+
 Vue.use(JX3BOX_UI);
 
 // 数据与路由
@@ -17,15 +19,15 @@ import store from "../store/knowledge";
 import App from "./Knowledge.vue";
 
 new Vue({
-    router: router,
-    store,
-    render: h => h(App),
+  router: router,
+  store,
+  render: h => h(App),
 }).$mount("#app");
 
 // 注册全局过滤器
 import * as filters from "../filters";
 
 Object.keys(filters).forEach((key) => {
-    Vue.filter(key, filters[key]);
+  Vue.filter(key, filters[key]);
 });
 
